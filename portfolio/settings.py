be24fbcbdf7dfef5237a724fda2f484dd2ee169a
Nagesh <Nageshnagansur@gmail.com>
2020-09-03 12:40:20 +0000

    removed old stuff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '41urfwr#i+iz$g*n-uoswz=mbf2v1n2xl(i=35euhq^df!m_pa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nagansurnagesh.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
