cda53271eeae53ebc250a5e6d4b57e3aa5991d97
Nagesh-Nagansur <nageshnagansur@gmail.com>
2020-09-03 18:27:02 +0530

    added local_settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL =  '/media/'
+
+
+try:
+    from . local_settings import *
+
+except ImportError:
+    print("looks like no local file you must be on live site")
